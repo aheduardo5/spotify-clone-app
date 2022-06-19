2aa8837091c62155ff0652016b8fdb2a7be3888b
Eduardo Aguilar <aheduardo5@gmail.com>
2022-06-19 00:07:49 -0500

    spotify.js pending to work

---
+++ src/spotify.js
@@ -0,0 +1,4 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+// Replace with your app's client ID, redirect URI and desired scopes
+const redirectUri = "http://localhost:3000/";
